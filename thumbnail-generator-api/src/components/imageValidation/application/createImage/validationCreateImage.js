fd7917e131b09a9dce1f4721f5afdfc8331997d9
julian <julian.merlo@naranja.com>
2020-08-12 13:37:02 -0300

    fix: create promise.all for array the images

--- thumbnail-generator-api/src/components/imageValidation/application/createImage/validationCreateImage.js
+++ thumbnail-generator-api/src/components/imageValidation/application/createImage/validationCreateImage.js
@@ -1,12 +1,16 @@
 const Joi = require("@hapi/joi");
 
 const schemaValidation = Joi.object({
-  path: Joi.string().required(),
-  name: Joi.string().required(),
-  size: Joi.number().required(),
-  type: Joi.string().required(),
+  arrayImages: Joi.array().items({
+    path: Joi.string().required(),
+    size: Joi.number().required(),
+    type: Joi.string().required(),
+  }),
 });
 
 module.exports = {
   schemaValidation,
 };
+
+
+
--- thumbnail-generator-api/src/components/imageValidation/domain/createImage/createImage.domain.js
+++ thumbnail-generator-api/src/components/imageValidation/domain/createImage/createImage.domain.js
@@ -1,26 +1,30 @@
 const {getDynamoInstance} = require("../../../../commons/aws/dynamoDb/config");
-const { images } = require("../../../../commons/utils/index");
+const { newImages } = require("../../../../commons/utils/index");
 const { v4: uuid } = require("uuid");
 const dynamoDb = require("../../../../commons/aws/dynamoDb/index")(
   "load-image-api-develop-image-table",
   getDynamoInstance()
 );
 
-const createImageDomain = async (image) => {
-  try {
-    if (image.size > 5120) {
-      const error = new Error();
-      error.code = "validation_size";
-      throw error;
-    }
-    //added id and new image
-    image.id = uuid();
-    image.newImages = images;
-
-    return response = await dynamoDb.putItem(image);
-  } catch (error) {
+const createImageDomain = async (images) => {
+ 
+  if (images.size > 5120) {
+    const error = new Error();
+    error.code = "validation_size";
     throw error;
   }
+  
+  try {
+    return  await Promise.all(
+        images.arrayImages.map(image =>{
+        image.id = uuid();
+        image.newImages = newImages;
+        return dynamoDb.putItem(image);
+        })
+      );
+    } catch (error) {
+      throw error;
+    }
 };
 
 module.exports = {
