e6aa9690f985b721dbda5db4c21a148a7ebd9836
julian <julian.merlo@naranja.com>
2020-08-14 12:26:44 -0300

    fix: update docs

--- thumbnail-generator-api/src/components/imageValidation/application/createImage/validationCreateImage.js
+++ thumbnail-generator-api/src/components/imageValidation/application/createImage/validationCreateImage.js
@@ -5,6 +5,18 @@ const schemaValidation = Joi.object({
     path: Joi.string().required(),
     size: Joi.number().required(),
     type: Joi.string().required(),
+    userData: Joi.object({
+      given_name: Joi.string().optional(),
+      family_name: Joi.string().optional(),
+      nickname: Joi.string().optional(),
+      name: Joi.string().optional(),
+      picture: Joi.string().optional(),
+      locale: Joi.string().optional(),
+      updated_at: Joi.string().optional(),
+      email: Joi.string().optional(),
+      email_verified: Joi.boolean().optional(),
+      sub: Joi.string().optional(),
+    }).optional()
   }),
 });
 
